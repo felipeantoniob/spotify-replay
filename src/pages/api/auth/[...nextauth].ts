0c7aa78c048b6680abec6c14178653405a6e1fd1
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2022-09-21 22:30:25 +0800

    Add Spotify auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,30 +1,78 @@
-import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import NextAuth, { User, type NextAuthOptions } from 'next-auth'
+import SpotifyProvider from 'next-auth/providers/spotify'
 
 // Prisma adapter for NextAuth, optional and can be removed
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "../../../server/db/client";
-import { env } from "../../../env/server.mjs";
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { prisma } from '../../../server/db/client'
+import { env } from '../../../env/server.mjs'
+
+const scopes = [
+  // "ugc-image-upload",
+  // "user-read-playback-state",
+  // "user-modify-playback-state",
+  // "user-read-currently-playing",
+  // "streaming",
+  // "app-remote-control",
+  'user-read-email',
+  'user-read-private',
+  // "playlist-read-collaborative",
+  'playlist-modify-public',
+  'playlist-read-private',
+  'playlist-modify-private',
+  // "user-library-modify",
+  // "user-library-read",
+  'user-top-read',
+  // "user-read-playback-position",
+  'user-read-recently-played',
+  'user-follow-read',
+  // "user-follow-modify",
+]
+
+const SPOTIFY_AUTHORIZATION_URL = `https://accounts.spotify.com/authorize?scope=${scopes.join(
+  '%20'
+)}`
 
 export const authOptions: NextAuthOptions = {
-  // Include user.id on session
-  callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
-      return session;
-    },
+  debug: false, // Logs oauth callback response to console
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+    maxAge: 60 * 30, // 30 minutes
   },
-  // Configure one or more authentication providers
+  jwt: {
+    secret: process.env.NEXTAUTH_SECRET,
+    maxAge: 60 * 30, // 30 minutes
+  },
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    SpotifyProvider({
+      clientId: String(env.SPOTIFY_CLIENT_ID),
+      clientSecret: String(env.SPOTIFY_CLIENT_SECRET),
+      authorization: SPOTIFY_AUTHORIZATION_URL,
     }),
-    // ...add more providers here
   ],
-};
+  callbacks: {
+    async jwt({ account, token }) {
+      if (account) {
+        token.accessToken = account.access_token
+        token.refreshToken = account.refresh_token
+      }
+      return token
+    },
+    async session({ session, token }) {
+      session.user = token.user as User
+      session.accessToken = token.accessToken
+      session.refreshToken = token.refreshToken
+      session.error = token.error
 
-export default NextAuth(authOptions);
+      return session
+    },
+  },
+  theme: {
+    colorScheme: 'dark',
+    // brandColor: '', // Hex color code
+    // logo: '', // Absolute URL to image
+  },
+}
+
+export default NextAuth(authOptions)
