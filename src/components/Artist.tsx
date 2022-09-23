daa00bf915d4313cccdbfd5725e2f7f13ba099ff
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2022-09-23 12:00:48 +0800

    Add Artist and ArtistModal

---
+++ src/components/Artist.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import useModal from '../hooks/useModal'
+import ArtistModal from './ArtistModal'
+
+const Artist = ({ ...artist }: SpotifyApi.ArtistObjectFull) => {
+  const { isModalShowing, toggleModal } = useModal()
+
+  return (
+    <>
+      <div className="mb-12 text-center">
+        <Image
+          src={artist.images[0] ? artist.images[0].url : ''}
+          alt="profile picture"
+          draggable="false"
+          height={200}
+          width={200}
+          onClick={toggleModal}
+          className="img-artist-profile cursor-pointer rounded-full object-cover transition-all hover:opacity-50"
+        />
+        <h3 className="pt-4 text-gray-200">{artist.name}</h3>
+      </div>
+      <ArtistModal show={isModalShowing} handleClose={toggleModal} artist={artist} />
+    </>
+  )
+}
+
+export default Artist
