11418d8e76c6db892b0259e5f6c43f78c8be43de
Felipe Buencamino <73531907+felipeantoniob@users.noreply.github.com>
2023-10-26 23:43:40 +0800

    Remove @next/font (#14)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { Inter } from '@next/font/google'
+import { Inter } from 'next/font/google'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { httpBatchLink } from '@trpc/client/links/httpBatchLink'
