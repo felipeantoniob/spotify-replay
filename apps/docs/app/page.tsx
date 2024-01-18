73e2c671e40773ccc0e08470fa888a55a6070f53
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2024-01-18 21:59:58 +0800

    Update package name

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
+import { Card } from "@spotify-replay/ui/card";
+import { Code } from "@spotify-replay/ui/code";
 import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
+import { Button } from "@spotify-replay/ui/button";
 
 function Gradient({
   conic,
