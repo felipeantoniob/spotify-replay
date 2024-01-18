89f80e5181bc34b6709ada72d9ff470b46e92af0
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2024-01-18 23:21:38 +0800

    Setup eslint tooling

--- apps/docs/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ["@spotify-replay/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-};
