4b27d6c7db285ad0a9fb23441b74931b4f2b4796
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2024-01-20 23:57:39 +0800

    Add playlist api functions

--- packages/api/src/router/spotify.ts
+++ packages/api/src/router/spotify.ts
@@ -1,4 +1,5 @@
-import type { MaxInt } from "@spotify/web-api-ts-sdk";
+import type { MaxInt, Track } from "@spotify/web-api-ts-sdk";
+import dayjs from "dayjs";
 import { z } from "zod";
 
 import spotifySdk from "@spotify-replay/auth/sdk";
@@ -14,9 +15,9 @@ const timeRangeSchema = z.enum(["long_term", "medium_term", "short_term"]);
 export const spotifyRouter = createTRPCRouter({
   hello: protectedProcedure
     .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
+    .query(({ input: { text } }) => {
       return {
-        greeting: `Hello ${input.text}`,
+        greeting: `Hello ${text}`,
       };
     }),
 
@@ -27,8 +28,7 @@ export const spotifyRouter = createTRPCRouter({
         limit: z.number(),
       }),
     )
-    .query(async ({ input }) => {
-      const { timeRange: time_range, limit } = input;
+    .query(async ({ input: { timeRange: time_range, limit } }) => {
       const response = await spotifySdk.currentUser.topItems(
         "artists",
         time_range,
@@ -45,8 +45,7 @@ export const spotifyRouter = createTRPCRouter({
         limit: z.number(),
       }),
     )
-    .query(async ({ input }) => {
-      const { timeRange: time_range, limit } = input;
+    .query(async ({ input: { timeRange: time_range, limit } }) => {
       const response = await spotifySdk.currentUser.topItems(
         "tracks",
         time_range,
@@ -62,23 +61,9 @@ export const spotifyRouter = createTRPCRouter({
     return response;
   }),
 
-  getPlaylist: protectedProcedure
-    .input(
-      z.object({
-        playlistId: z.string(),
-      }),
-    )
-    .query(async ({ input }) => {
-      const { playlistId } = input;
-      const response = await spotifySdk.playlists.getPlaylist(playlistId);
-
-      return response;
-    }),
-
   getGenreData: protectedProcedure
     .input(z.object({ timeRange: timeRangeSchema }))
-    .query(async ({ input }) => {
-      const { timeRange } = input;
+    .query(async ({ input: { timeRange } }) => {
       const results = await spotifySdk.currentUser.topItems(
         "artists",
         timeRange,
@@ -91,5 +76,80 @@ export const spotifyRouter = createTRPCRouter({
       const topGenresArray = getTopGenres(genreFrequencyArray, 20);
       const genresArtistsObject = getGenreChartData(topGenresArray, topArtists);
       return genresArtistsObject;
+    }),
+
+  getPlaylist: protectedProcedure
+    .input(
+      z.object({
+        playlistId: z.string(),
+      }),
+    )
+    .query(async ({ input: { playlistId } }) => {
+      const response = await spotifySdk.playlists.getPlaylist(playlistId);
+
+      return response;
+    }),
+
+  createPlaylist: protectedProcedure
+    .input(
+      z.object({
+        playlistName: z.string(),
+      }),
+    )
+    .mutation(async ({ input }) => {
+      const { playlistName } = input;
+      const userProfile = await spotifySdk.currentUser.profile();
+      userProfile.id;
+      const response = await spotifySdk.playlists.createPlaylist(
+        userProfile.id,
+        {
+          name: `${playlistName} • ${dayjs().format("YYYY MMMM DD")}`,
+          description: `Playlist created on ${dayjs().format("MMMM D, YYYY h:mm A")}`,
+          public: false,
+        },
+      );
+
+      return response.id;
+    }),
+
+  addTracksToPlaylist: protectedProcedure
+    .input(
+      z.object({
+        playlistId: z.string(),
+        tracksUriArray: z.string().array(),
+      }),
+    )
+    .mutation(async ({ input: { playlistId, tracksUriArray } }) => {
+      const response = await spotifySdk.playlists.addItemsToPlaylist(
+        playlistId,
+        tracksUriArray,
+      );
+
+      return response;
+    }),
+
+  getMultipleArtistsTopTracks: protectedProcedure
+    .input(
+      z.object({
+        artistsIdArray: z.string().array(),
+        artistsLimit: z.number(),
+        tracksLimit: z.number(),
+      }),
+    )
+    .query(async ({ input: { artistsIdArray, artistsLimit, tracksLimit } }) => {
+      let topArtistTopTracks: Track[] = [];
+
+      const filteredArtistsIdArray = artistsIdArray.slice(0, artistsLimit);
+
+      for (const id of filteredArtistsIdArray) {
+        const response = await spotifySdk.artists.topTracks(id, "US");
+
+        const topTracks = response.tracks;
+
+        const filteredTopTracks = topTracks.slice(0, tracksLimit);
+
+        topArtistTopTracks = [...topArtistTopTracks, ...filteredTopTracks];
+      }
+      return topArtistTopTracks;
     }),
 });
