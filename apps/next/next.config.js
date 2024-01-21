3320c22875bc426157da8710f1496a1e54f6646e
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2024-01-21 11:32:31 +0800

    Update next config remotePatterns

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -7,24 +7,18 @@ const nextConfig = {
     "@spotify-replay/ui",
   ],
   images: {
-    domains: [
-      "i.scdn.co",
-      "mosaic.scdn.co",
-      "seeded-session-images.scdn.co",
-      "blend-playlist-covers.spotifycdn.com",
-      "lineup-images.scdn.co",
-      "newjams-images.scdn.co",
-      "thisis-images.scdn.co",
-      "placehold.co",
-    ],
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**.scdn.com",
+        hostname: "**.scdn.co",
       },
       {
         protocol: "https",
         hostname: "**.spotify.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.spotifycdn.com",
       },
     ],
   },
