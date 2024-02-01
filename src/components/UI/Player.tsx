cb2afad50e3e1876a9c303b0267504c7e769d26e
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2024-02-01 10:22:13 +0800

    Refactor player styles

--- src/components/UI/Player.tsx
+++ src/components/UI/Player.tsx
@@ -1,11 +1,20 @@
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
-import SpotifyPlayer from 'react-spotify-web-playback'
+import SpotifyPlayer, { type StylesOptions } from 'react-spotify-web-playback'
 
 import { Colors } from '../../constants/Colors'
 import { useBoundStore } from '../../store'
 
 const ROUTERS_WITH_PLAYER = ['/profile', '/artists', '/tracks', '/genres']
+
+const PlayerStyles: Partial<StylesOptions> = {
+  bgColor: Colors['primary-container'],
+  color: Colors['on-primary-container'],
+  loaderColor: Colors['on-primary-container'],
+  sliderColor: Colors['on-primary-container'],
+  trackArtistColor: Colors['on-primary-container'],
+  trackNameColor: Colors['on-primary-container'],
+}
 
 const Player = () => {
   const router = useRouter()
@@ -22,21 +31,13 @@ const Player = () => {
         <div className="mx-auto w-full md:pl-24">
           <div className="mx-auto mb-20 max-w-3xl bg-blue-500 md:mb-0">
             <SpotifyPlayer
-              autoPlay
               callback={(state) => setIsPlaying(state.isPlaying)}
               initialVolume={0.5}
               token={accessToken}
               play={isPlaying}
               uris={uri ? [`spotify:track:${uri}`] : ['']}
               showSaveIcon
-              styles={{
-                bgColor: Colors['primary-container'],
-                color: Colors['on-primary-container'],
-                loaderColor: Colors['on-primary-container'],
-                sliderColor: Colors['on-primary-container'],
-                trackArtistColor: Colors['on-primary-container'],
-                trackNameColor: Colors['on-primary-container'],
-              }}
+              styles={PlayerStyles}
             />
           </div>
         </div>
