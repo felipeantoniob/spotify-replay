9f9d57759500e338c7080d7bae820c96bb77cab6
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2024-08-01 13:09:11 +0800

    Update dependencies
    Update dependencies
    
    Remove react-spotify-web-playback patch
    
    Update husky pre-commit
    
    Update storybook
    
    Update dependencies
    
    Update dependencies
    
    Update dependencies
    
    Update dependencies
    
    Update dependencies
    
    Update dependencies
    
    Update dependencies
    
    Update dependencies
    
    Update dependencies
    
    Update react
    
    Update trpc
    
    Update next
    
    Remove remix typecheck

--- apps/next/src/components/AuthSessionProvider.tsx
+++ apps/next/src/components/AuthSessionProvider.tsx
@@ -1,17 +1,20 @@
 "use client";
 
 import type { Session } from "next-auth";
-import type { ReactNode } from "react";
 import { SessionProvider } from "next-auth/react";
 
 function AuthSessionProvider({
   session,
   children,
 }: {
-  children: ReactNode;
+  children: React.ReactNode;
   session: Session | null | undefined;
 }) {
-  return <SessionProvider session={session}>{children}</SessionProvider>;
+  return (
+    <SessionProvider session={session}>
+      <>{children}</>
+    </SessionProvider>
+  );
 }
 
 export default AuthSessionProvider;
